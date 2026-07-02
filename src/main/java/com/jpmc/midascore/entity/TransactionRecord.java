135a05dd39115eade1d5a213fc6e293d5738021e
Priyansh Pandey <pandeypriyansh33@gmail.com>
2026-07-02 13:48:40 -0700

    Create TransactionRecord entity for transactions
    Added TransactionRecord entity to represent transactions between users.

---
+++ src/main/java/com/jpmc/midascore/entity/TransactionRecord.java
@@ -0,0 +1,29 @@
+package com.jpmc.midascore.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class TransactionRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private UserRecord sender;
+
+    @ManyToOne
+    private UserRecord recipient;
+
+    private float amount;
+
+    // Default constructor required by JPA
+    public TransactionRecord() {}
+
+    public TransactionRecord(UserRecord sender, UserRecord recipient, float amount) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.amount = amount;
+    }
+
+    // Getters and Setters
+    public Long getId() { return id; }
